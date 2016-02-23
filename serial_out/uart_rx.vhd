a266521810c92d746ee34c67887fd3cfddac8540
Arthur Benemann <arthur.benemann@gmail.com>
2016-02-23 13:42:23 -0800

    remove unused register in RX shift reg

--- serial_out/uart_rx.vhd
+++ serial_out/uart_rx.vhd
@@ -16,7 +16,7 @@ architecture Behavioral of uart_rx is
 	constant baudrate : real := 115.2; -- kbits/s
 	
 	signal baud_counter : std_logic_vector(6 downto 0) := (others => '0');	
-	signal sr : std_logic_vector(39 downto 0) := (others => '1');
+	signal sr : std_logic_vector(38 downto 0) := (others => '1');
 begin
 	
 	process (clk) begin
@@ -24,19 +24,19 @@ begin
 			if baud_counter = integer(clock_freq*1000.0/baudrate/4.0 - 1.0) then
 				baud_counter <= (others => '0');
 				
-				sr <= rx & sr(39 downto 1);	-- shift over-sampled data into shift_reg
+				sr <= rx & sr(38 downto 1);	-- shift over-sampled data into shift_reg
 				
-				if((sr(38) and  sr(37)) and	-- stop bit
-				   (sr(34) xnor sr(33)) and	-- MSB
-				   (sr(30) xnor sr(29)) and
-				   (sr(26) xnor sr(25)) and
-				   (sr(22) xnor sr(21)) and
-				   (sr(18) xnor sr(17)) and
-				   (sr(14) xnor sr(13)) and
-				   (sr(10) xnor sr(09)) and
-				   (sr(06) xnor sr(05)) and	-- LSB
-				   (sr(02) nor  sr(01))) = '1' then -- start bit
-						rx_data <= sr(34)&sr(30)&sr(26)&sr(22)&sr(18)&sr(14)&sr(10)&sr(6);
+				if((sr(37) and  sr(36)) and	-- stop bit
+				   (sr(33) xnor sr(32)) and	-- MSB
+				   (sr(29) xnor sr(28)) and
+				   (sr(25) xnor sr(24)) and
+				   (sr(21) xnor sr(20)) and
+				   (sr(17) xnor sr(16)) and
+				   (sr(13) xnor sr(12)) and
+				   (sr(09) xnor sr(08)) and
+				   (sr(05) xnor sr(04)) and	-- LSB
+				   (sr(01) nor  sr(00))) = '1' then -- start bit
+						rx_data <= sr(33)&sr(28)&sr(24)&sr(20)&sr(16)&sr(12)&sr(8)&sr(4);
 						rx_ready <= '1';
 						sr <= (others => '1');
 				else
