b5453baf6717fb7a8e110be5dde1e43e7e400c62
Arthur Benemann <arthur.benemann@gmail.com>
2016-02-26 14:37:11 -0800

    fm_transmitter: use IP adder core for better timing constrains

--- fm_transmitter/fm_modulator.vhd
+++ fm_transmitter/fm_modulator.vhd
@@ -17,26 +17,35 @@ architecture Behavioral of fm_modulator is
 	
 	constant center_freq : signed(31 downto 0) := to_signed(integer(accumulator_size*fm_frequency/input_clk),32);
 	
+	component phase_adder port (
+		a,b : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		s : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	end component;
 	
-	signal phase_shift_data: signed (31 downto 0) := (others => '0');
-	signal phase_shift: signed (31 downto 0) := (others => '0');
-	signal phase_accumulator : signed (31 downto 0) := (others => '0');
+	
+	signal ph_shift_data,ph_shift,ph_accumulator: signed (31 downto 0) := (others => '0');
+	signal sum : std_logic_vector (31 downto 0);
 
 begin
 	
 	process (clk) begin
 		if rising_edge(clk) then
-			phase_shift_data <= resize(data & "0000",32);
-			phase_shift <= center_freq + phase_shift_data;				
+			ph_shift_data <= resize(data & "0000",32);
+			ph_shift <= center_freq + ph_shift_data;				
 		end if;
 	end process;
+	
+	fast_adder : phase_adder port map (  -- the adder IP is required to run this sum at 320MHz
+		a => std_logic_vector(ph_accumulator), b => std_logic_vector(ph_shift),
+		s => sum
+  );
 	
 	process (clk_modulator) begin
 		if rising_edge(clk_modulator) then
-			phase_accumulator <= phase_accumulator + phase_shift;						
+			ph_accumulator <= signed(sum);						
 		end if;
 	end process;
 	
-	fm_out <= std_logic(phase_accumulator(31));
+	fm_out <= std_logic(ph_accumulator(31));
 
 end Behavioral;
