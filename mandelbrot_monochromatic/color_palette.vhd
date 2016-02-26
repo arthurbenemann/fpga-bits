440c12e20a391ac0b5b417ee1c2b0b0c0e0cefc9
Arthur Benemann <arthur.benemann@gmail.com>
2016-02-25 20:08:34 -0800

    mandelbrot: make color pallet use less memory, and be circular

--- mandelbrot_monochromatic/color_palette.vhd
+++ mandelbrot_monochromatic/color_palette.vhd
@@ -1,6 +1,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 entity color_palette is Port ( 
 	clk : in std_logic;
@@ -8,21 +9,34 @@ entity color_palette is Port (
    color_rgb : out  STD_LOGIC_VECTOR (11 downto 0));
 end color_palette;
 
+
 architecture Behavioral of color_palette is
- signal color_buff : STD_LOGIC_VECTOR (11 downto 0) :=(others=>'0');
+ signal color_buff : unsigned (1 downto 0) :=(others=>'0');
+ signal temp : natural := 0;
+ 
+	function count_ones(s : std_logic_vector) return integer is
+	  variable temp : natural := 0;
+	begin
+	  for i in s'range loop
+		 if s(i) = '1' then temp := temp + 1; 
+		 end if;
+	  end loop;
+	  return temp;
+	end function count_ones;
+
 begin
 
 --	color_rgb <= color_buff; -- monochromatic mapping
-	with color_buff(3 downto 0) select color_rgb <= -- rgb palette
-		x"00f" when "1000",
-		x"0f0" when "1100",
-		x"f00" when "1110",
-		x"fff" when "1111",
+	with color_buff select color_rgb <= -- rgb palette
+		x"00f" when "00",
+		x"0f0" when "01",
+		x"f00" when "10",
+		x"fff" when "11",
 		x"000" when others;
 		
 	process (clk) begin
 		if rising_edge(clk) then
-			color_buff <= color(19 downto 8);
+			color_buff <= to_unsigned(count_ones(color),2);
 		end if;
 	end process;	
 
