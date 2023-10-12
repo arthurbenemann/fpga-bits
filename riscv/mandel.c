65779e3997dc2aa16a7739cf5bf6a0f7965c4347
arthurbenemann <arthur.benemann@gmail.com>
2023-10-11 23:39:23 -0700

    adjust plot

--- riscv/mandel.c
+++ riscv/mandel.c
@@ -10,15 +10,15 @@
 #define IO_IN(port) *(volatile uint32_t *)(IO_BASE + port)
 #define IO_OUT(port, val) *(volatile uint32_t *)(IO_BASE + port) = (val)
 
-#define W 46
-#define H 46
+#define W 300
+#define H 200
 
 #define mandel_shift 10
 #define mandel_mul (1 << mandel_shift)
 #define xmin -2 * mandel_mul
 #define ymax 2 * mandel_mul
 #define ymin -2 * mandel_mul
-#define xmax 2 * mandel_mul
+#define xmax 2 * mandel_mul/2
 #define dx (xmax - xmin) / H
 #define dy (ymax - ymin) / H
 #define norm_max (4 << mandel_shift)
@@ -26,29 +26,30 @@
 #define ANSIRGB(R, G, B) "\033[48;2;" #R ";" #G ";" #B "m  "
 
 const char *colormap[21] = {
-    ANSIRGB(0, 0, 0),
-    ANSIRGB(0, 0, 40),
-    ANSIRGB(0, 0, 80),
-    ANSIRGB(0, 0, 120),
-    ANSIRGB(0, 0, 160),
-    ANSIRGB(0, 0, 200),
-    ANSIRGB(0, 0, 240),
+    ANSIRGB(  0,   0,   0),
+    ANSIRGB(250, 250, 250),
+    ANSIRGB(220, 200, 240),
+    ANSIRGB(200, 180, 230),
+    ANSIRGB(180, 160, 220),
+    ANSIRGB(160, 140, 210),
+    ANSIRGB(140, 120, 200),
 
-    ANSIRGB(0, 0, 0),
-    ANSIRGB(0, 40, 0),
-    ANSIRGB(0, 80, 0),
-    ANSIRGB(0, 120, 0),
-    ANSIRGB(0, 160, 0),
-    ANSIRGB(0, 200, 0),
-    ANSIRGB(0, 240, 0),
+    ANSIRGB(100, 100, 190),
+    ANSIRGB( 80,  80, 180),
+    ANSIRGB( 60,  60, 170),
+    ANSIRGB( 40,  40, 160),
+    ANSIRGB( 30,  30, 150),
+    ANSIRGB( 20,  20, 140),
+    ANSIRGB( 10,  10, 130),
 
-    ANSIRGB(0, 0, 0),
-    ANSIRGB(40, 0, 0),
-    ANSIRGB(80, 0, 0),
-    ANSIRGB(120, 0, 0),
-    ANSIRGB(160, 0, 0),
-    ANSIRGB(200, 0, 0),
-    ANSIRGB(240, 0, 0)};
+    ANSIRGB(  0,   0, 120),
+    ANSIRGB(  0,   0, 100),
+    ANSIRGB(  0,   0,  80),
+    ANSIRGB(  0,   0,  60),
+    ANSIRGB(  0,   0,  40),
+    ANSIRGB(  0,   0,  20),
+    ANSIRGB(  0,   0,   0)
+    };
 
 int main()
 {
@@ -81,7 +82,7 @@ int main()
                     }
                     --iter;
                 }
-                int color = (iter) % 21;
+                int color = (iter);
                 printf(color == last_color ? "  " : colormap[color]);
                 last_color = color;
                 Cr += dx;
